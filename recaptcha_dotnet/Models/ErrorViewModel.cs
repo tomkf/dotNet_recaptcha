a584275d372dbf66d3f68d576de15a5ac40bb34b
tomkf <thomaskeithferris@gmail.com>
2020-03-14 12:21:24 -0700

    Add project files.

---
+++ recaptcha_dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace recaptcha_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
